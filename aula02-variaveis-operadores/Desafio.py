653089b84cf5d32b12fc124f622d1ecfffb71a23
pedrosoaresdeosuza03-rgb <pedrosoaresdesouza03-rgb>
2026-03-17 11:41:09 -0300

    aula 02

---
+++ aula02-variaveis-operadores/Desafio.py
@@ -0,0 +1,14 @@
+print("Boas-vindas!")
+
+print(Boas-vindas+ nome)
+print("Boas-vindas! + nome")
+print("Boas-vindas"+"Nome")# De acordo com o valor
+
+#Comentários de 1 linha
+'''
+   comentários
+   linhas
+'''
+#variaveis
+Nome
+numero
---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,25 @@
+print("Ola mundo")
+
+print(7+4)
+print("7 + 4")
+print("7" + "4")#concatenação de strings
+
+#comentários de 1 linha
+'''
+   comentários de
+   multiplas
+   linhas
+   
+'''
+#variaveis
+nome = "Alexandre"
+idade = 25
+peso = 70.2
+
+print("oiii", nome, idade, peso)
+print(f"oiii, {nome}!")
+
+#input -- simula forms no cmd
+nome = input("Digite seu nome:")
+idade = int(input("Digite sua idade"))
+peso = float(input("Digite seu peso"))
