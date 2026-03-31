8d4c409d4ac03bd5c745b6981d526f947fb17ccd
pedrosoaresdesouza03-rgb <pedrosoaresdesouza03@gmail.com>
2026-03-31 11:42:17 -0300

    aula 03 módulos, funções, e condicionais

---
+++ aula02-variaveis-operadores/aula03-mudulos-condicionais/aula03b-funcoes.py
@@ -0,0 +1,15 @@
+def print_lyrics():
+    print("I aint't gonna forever")
+    print("I just want to live while I'm alive")
+
+print_lyrics()
+print_lyrics()
+# o que o Def ajuda
+def soma():
+    n1 = 4
+    n2 = 2
+    soma = n1 + n2
+    print(soma)
+
+print(soma)
+print(2345+3774)
